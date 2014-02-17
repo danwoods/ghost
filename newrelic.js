e84ec934048d09429f29c5247147173ce3b38e9a
Dan Woodson <woodson.dan@gmail.com>
2014-02-17 01:49:49 +0000

    setup new relic

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name : ['Ghost Blog'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : 'e0e2a1e5e15d958e37e50faf7405e13e73fef54f',
+  logging : {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level : 'trace'
+  }
+};
